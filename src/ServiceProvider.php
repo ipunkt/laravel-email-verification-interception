00b0d69b8c8ea86a5502f488ed82e52c1327ef51
Robert Kummer <rok@ipunkt.biz>
2016-11-18 14:19:37 +0100

    Basic setup done

---
+++ src/ServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Ipunkt\Laravel\EmailVerificationInterception;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+
+    }
+}
