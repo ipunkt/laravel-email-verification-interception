ecebe29fc3993566d8d5306b8d8aa19a530fa215
rokde <github@mail.robert-kummer.de>
2016-11-28 15:17:51 +0100

    Listener removed, function callback is good enough

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace Ipunkt\Laravel\EmailVerificationInterception;
 
+use Ipunkt\Laravel\EmailVerificationInterception\Models\Email;
+
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
     public function register()
@@ -14,8 +16,16 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
         /** @var \Illuminate\Events\Dispatcher $events */
         $events = $this->app['events'];
 
-        $events->listen(\Illuminate\Auth\Events\Registered::class, function () {
-            dd('registered', func_get_args());
+        $events->listen(\Illuminate\Auth\Events\Registered::class, function ($user) {
+            try {
+                if (isset($user->email) && !empty($user->email)) {
+                    Email::create([
+                        'user_id' => $user->id,
+                        'email' => $user->email,
+                    ]);
+                }
+            } catch (\Exception $e) {
+            }
         });
     }
 }
